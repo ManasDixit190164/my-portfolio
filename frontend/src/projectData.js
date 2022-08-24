8433b72a9f9b7ed763433406aaba67ef2ee367e3
manasdixit190164 <manasdixit@gmail.com>
2022-08-24 20:45:44 +0530

    first commit

---
+++ frontend/src/projectData.js
@@ -0,0 +1,21 @@
+const projects = [
+  {
+    title: 'CodeViral.in',
+    image: './images/rn-mockup',
+    description: 'Full featured ecommerce store built with MERN stack and Paypal payments API. Admin page to manage customers, products & orders.',
+    tools: ['React', 'Redux', 'MongoDB', 'JWT', 'Express'],
+    github: '#',
+    link: 'https://www.codeviral.in',
+  },
+  {
+    title: 'Resume Builder',
+    image: './images/recipeApp-mockup',
+    description: 'It is having the best temples for students, They have to just edit it and their resume is ready in 10 mins they can download in PDF format.',
+    tools: ['HTML','Javascript', 'Python'],
+    github: 'github.com/ManasDixit190164/Beautiful-Resume-Builder/',
+    link: 'https://manasdixit190164.github.io/Beautiful-Resume-Builder/',
+  },
+  
+];
+
+export default projects;
